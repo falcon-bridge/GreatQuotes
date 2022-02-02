8c01ee1e06d2169fd8049b9421c434c27bd10def
Aayush <falcon.bridge2000@gmail.com>
2022-02-02 12:18:59 +0530

    rendered data in details page when a specific quote is viewed

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -3,12 +3,33 @@ import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Aayush",
+    text: "Learning React is fun and I enjoy it a lot",
+  },
+  {
+    id: "q2",
+    author: "Hrithik",
+    text: "Learning how the Web works is fun",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found</p>;
+  }
+
   return (
     <Fragment>
-      <div>Quote detail page</div>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
