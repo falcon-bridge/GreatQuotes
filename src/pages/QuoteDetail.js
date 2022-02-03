29f2e143145cd98b3934798d9199397c2fa969d3
Aayush <falcon.bridge2000@gmail.com>
2022-02-03 12:11:41 +0530

    used more flexible route paths

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 
-import { Link, Route, useParams, useLocation } from "react-router-dom";
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -20,12 +20,7 @@ const DUMMY_QUOTES = [
 
 const QuoteDetail = () => {
   const params = useParams();
-  const location = useLocation();
-
-  console.log(location);
-  // const queryParams = new URLSearchParams(location.search);
-
-  // const hasComments = queryParams.get();
+  const match = useRouteMatch();
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
@@ -36,14 +31,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route exact path={`/quotes/${params.quoteId}`}>
+      <Route exact path={`${match.path}`}>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
