45811d9cc94cf84b4f7d49c124644e8903aaa3f2
Aayush <falcon.bridge2000@gmail.com>
2022-02-03 15:59:19 +0530

    fetched data needed for quote detail page from the firebase

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,36 +1,65 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 
 import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
-const DUMMY_QUOTES = [
-  {
-    id: "q1",
-    author: "Aayush",
-    text: "Learning React is fun and I enjoy it a lot",
-  },
-  {
-    id: "q2",
-    author: "Hrithik",
-    text: "Learning how the Web works is fun",
-  },
-];
+// const DUMMY_QUOTES = [
+//   {
+//     id: "q1",
+//     author: "Aayush",
+//     text: "Learning React is fun and I enjoy it a lot",
+//   },
+//   {
+//     id: "q2",
+//     author: "Hrithik",
+//     text: "Learning how the Web works is fun",
+//   },
+// ];
 
 const QuoteDetail = () => {
   const params = useParams();
   const match = useRouteMatch();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = params;
 
-  if (!quote) {
-    return <p>No quote found</p>;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote);
+
+  // const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  console.log(loadedQuote);
+  if (!loadedQuote) {
+    return <p className="centered">No quote found</p>;
   }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route exact path={`${match.path}`}>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
