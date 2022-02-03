374e2751d8ecea022867a072baf47a585fd3843a
Aayush <falcon.bridge2000@gmail.com>
2022-02-03 14:40:31 +0530

    send HTTP request to store required data on firebase after submitting the form

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,17 +1,31 @@
+import { useEffect } from "react";
+
 import { useHistory } from "react-router-dom";
 
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
   const history = useHistory();
+  const { sendRequest, status } = useHttp(addQuote);
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
 
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+    // console.log(quoteData);
+    sendRequest(quoteData);
 
     // return <Redirect to="/quotes" />;
-    history.push("/quotes");
+    // history.push("/quotes");
   };
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
